f70d231727f6ce902b25e1f5be5c8354e52c6bf5
Ermiyas <98635649+ermiyasmesfin@users.noreply.github.com>
2022-05-16 20:09:41 +0300

    Update and rename 0-main.c to main.c

--- test/main.c
+++ test/main.c
@@ -7,6 +7,7 @@
  *
  * Return: Always 0
  */
+
 int main(void)
 {
 	int len;
@@ -14,9 +15,8 @@ int main(void)
 	unsigned int ui;
 	void *addr;
 
-
-	len = _printf("%c%cth %s%s a%cg%s: Y%sou %s no%ching%s Snow.%c", 'W', 'i', "some ", "more", 'r', "s", "", "know", 't', ", Jon", '\n');
-	len2 = printf("%c%cth %s%s a%cg%s: Y%sou %s no%ching%s Snow.%c", 'W', 'i', "some ", "more", 'r', "s", "", "know", 't', ", Jon", '\n');
+	len = _printf("Let's try to printf a simple sentence.\n");
+	len2 = printf("Let's try to printf a simple sentence.\n");
 	ui = (unsigned int)INT_MAX + 1024;
 	addr = (void *)0x7ffe637541f0;
 	_printf("Length:[%d, %i]\n", len, len);
@@ -40,14 +40,6 @@ int main(void)
 	_printf("Len:[%d]\n", len);
 	printf("Len:[%d]\n", len2);
 	_printf("Unknown:[%r]\n");
-	//printf("Unknown:[%r]\n");
-	_printf("%d + %d = %d\n", INT_MIN, INT_MAX, (INT_MIN + INT_MAX));
-	printf("%i\n", _printf("%b\n", -10));
-	_printf("There is %b bytes in %b KB\n", 1024, 1);
-	_printf("%u == %o == %x == %X\n", 1024, 1024, 1024, 1024);
-	_printf("%u\n", UINT_MAX);
-	printf("%u\n", UINT_MAX);
-	 _printf("Could you print some non-prntable characters?\n%S\nThanks!\n", "Sure:\x1F\x7F\n");
-	 _printf("%b\n", 1024);
+	printf("Unknown:[%r]\n");
 	return (0);
 }
